c96f055ba5bee9d4864d251912905d2cc4f303f4
pancia <anthony.dayzerostudio@gmail.com>
2014-11-24 03:18:02 -0800

    Added hanoi function, & moved to Ex1.hs

--- Ex1.hs
+++ Ex1.hs
@@ -18,3 +18,14 @@ sumDigits = foldl (+) 0
 validate :: Integer -> Bool
 validate i = (==0) $ rem sum 10
     where sum = sumDigits . doubleEveryOther . toDigits $ i
+
+type Peg = String
+type Move = (Peg, Peg)
+type NumDisks = Integer
+hanoi :: NumDisks -> Peg -> Peg -> Peg -> [Move]
+hanoi n a b c
+    | n < 1 = []
+    | n == 0 = [(a, b)]
+    | otherwise = (hanoi (n-1) a c b) ++ [(a, b)] ++ (hanoi (n-1) c b a)
+
+
