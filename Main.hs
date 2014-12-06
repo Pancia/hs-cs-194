bc79c58decc2324af3b89233ec24255ba020a65d
pancia <anthony.dayzerostudio@gmail.com>
2014-12-05 16:12:00 -0800

    Added type decl and simplified some lines.

--- Main.hs
+++ Main.hs
@@ -6,15 +6,20 @@ data Cell = Cell { x :: Int
                  , t :: Char
                  } deriving (Show)
 
-genBoard w h = [(Cell x y 'X')
-               | y <- [1..h], x <- [1..w]]
+type Board = [Cell]
 
+genBoard :: Int -> Int -> Board
+genBoard w h = [Cell x y 'X'
+               | y <- [1..h]
+               , x <- [1..w]]
+
+printMaze :: Board -> IO ()
 printMaze board =
         let l = last board
             w = x l
         in putStr . unlines
-                  . (map (intersperse ' '))
-                  . (chunksOf w)
-                  . (map t)
+                  . map (intersperse ' ')
+                  . chunksOf w
+                  . map t
                   $ board
 
