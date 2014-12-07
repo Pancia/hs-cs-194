1286a9035ce84d5cd1642c82d056870ada8519b1
pancia <anthony.dayzerostudio@gmail.com>
2014-12-07 05:36:43 -0800

    Added shuffle, renamed Cell accessors, & added a whole bunch of helper functions.

--- Main.hs
+++ Main.hs
@@ -1,37 +1,112 @@
 import Data.List
 import Data.List.Split
 import System.Random (randomRIO)
+import Data.Array.IO
+import Control.Monad
 
-data Cell = Cell { x :: Int
-                 , y :: Int
-                 , t :: Char
+shuffle :: [a] -> IO [a]
+shuffle xs = do
+        ar <- newArray n xs
+        forM [1..n] $ \i -> do
+            j <- randomRIO (i,n)
+            vi <- readArray ar i
+            vj <- readArray ar j
+            writeArray ar j vi
+            return vj
+    where
+        n = length xs
+        newArray :: Int -> [a] -> IO (IOArray Int a)
+        newArray n xs =  newListArray (1,n) xs
+
+data Cell = Cell { getX :: Int
+                 , getY :: Int
+                 , getType :: Char
                  } deriving (Show)
 
-type Board = [Cell]
+type Board      = [Cell]
+type Candidates = [Cell]
+type Neighbors  = [Cell]
 
 genBoard :: Int -> Int -> Board
 genBoard w h = [Cell x y 'X'
                | y <- [1..h]
                , x <- [1..w]]
 
+cellToTuple :: Cell -> ((Int, Int), Char)
+cellToTuple (Cell x y t) = ((x, y), t)
+
 printMaze :: Board -> IO ()
 printMaze board =
         let l = last board
-            w = x l
+            w = getX l
         in putStr . unlines
                   . map (intersperse ' ')
                   . chunksOf w
-                  . map t
+                  . map getType
                   $ board
 
-updateInBoard :: (Char -> Char) -> Int -> Int -> Board -> Board
-updateInBoard _ _ _ []       = []
-updateInBoard f x y (c@(Cell x' y' t):cells)
+updateInBoard :: (Char -> Char) -> Board -> Cell -> Board
+updateInBoard _ [] _         = []
+updateInBoard f (c'@(Cell x' y' t):cells) c@(Cell x y _)
         | x == x' && y == y' = Cell x y (f t) : cells
-        | otherwise          = c : updateInBoard f x y cells
+        | otherwise          = c' : updateInBoard f cells c
 
-pick :: Board -> IO Cell
-pick cells = randomRIO (0, length cells - 1)
-             >>= return . (cells !!)
+floorIt :: Board -> Cell -> Board
+floorIt = updateInBoard (const '_')
 
+randNth :: [a] -> IO a
+randNth l = randomRIO (0, length l - 1)
+            >>= return . (l !!)
 
+getRadialNeighbors :: Board -> Cell -> Neighbors
+getRadialNeighbors b (Cell x y _)
+            = [Cell x' y' (getT x' y')
+              | dx <- [-1..1], dy <- [-1..1],
+              let x' = (x+dx), let y' = (y+dy)]
+            where getT :: Int -> Int -> Char
+                  getT i j = getType (b' !! (i-1) !! (j-1))
+                  b' = chunksOf (truncate
+                                . sqrt
+                                . fromIntegral
+                                . length $ b) b
+
+onSameAxis :: Cell -> Cell -> Cell -> Bool
+onSameAxis orig@(Cell x y _) trgt@(Cell i j _) cell@(Cell x' y' _)
+        | y == j    = x == x'
+        | x == i    = y == y'
+        | otherwise = error "onSameAxis: orig & trgt must be adj"
+
+getNeighbors :: Board -> Cell -> Cell -> Neighbors
+getNeighbors b orig@(Cell x y _) trgt@(Cell i j _)
+        = filter (not . onSameAxis orig trgt)
+                 $ getRadialNeighbors b trgt
+
+getNeighborsOfType :: Board -> Cell -> Cell -> Char -> Neighbors
+getNeighborsOfType b orig trgt t = filter ((t ==) . getType)
+                                          $ getNeighbors b orig trgt
+
+getWallNeighbors :: Board -> Cell -> Cell -> Neighbors
+getWallNeighbors b o t = getNeighborsOfType b o t 'X'
+
+getAValidNeighbor :: Board -> Cell -> Neighbors -> Cell
+getAValidNeighbor _ _ [] = error "derp"
+getAValidNeighbor board mark neighbors =
+        if 5 == cWallCount
+            then c
+            else getAValidNeighbor board mark cs
+        where cWallCount = 5 --length $ getWallNeighbors board mark c
+              (c:cs) = neighbors
+
+main :: IO ()
+--main = randNth board
+--            >>= return . (updateInBoard (const '_') board)
+--            >>= printMaze
+--            >>  putStr "\n"
+main = shuffle (getNeighbors b c1 c2) >>=
+       return . (getAValidNeighbor b (Cell 3 3 '?')) >>=
+       printCell
+       where b = genBoard 5 5
+             c1 = (Cell 3 3 '*')
+             c2 = (Cell 3 4 '*')
+             printCell :: Cell -> IO ()
+             printCell = print
