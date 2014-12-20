20a14ac9d98940abd4709716477f290c69e3a30f
Pancia <anthony.dayzerostudio@gmail.com>
2014-12-20 01:44:34 -0800

    Finished exercise 5.

--- ex5/Ex5.hs
+++ ex5/Ex5.hs
@@ -21,12 +21,12 @@ instance Ring Mod5 where
 instance Parsable Mod5 where
         parse = listToMaybe . reads
 
-mod5ParsingWorks :: Bool
-mod5ParsingWorks = (parse "MkMod 3" == Just (MkMod 3 :: Mod5, "")) &&
-                   (parseRing "MkMod 3 + MkMod 2 * MkMod 1" == Just (MkMod 0 :: Mod5)) &&
-                   (addId == (MkMod 0 :: Mod5)) &&
-                   (mulId == (MkMod 1 :: Mod5)) &&
-                   (addInv (MkMod 2) == (MkMod 3 :: Mod5))
+test_mod5 :: Bool
+test_mod5 = (parse "MkMod 3" == Just (MkMod 3 :: Mod5, "")) &&
+            (parseRing "MkMod 3 + MkMod 2 * MkMod 1" == Just (MkMod 0 :: Mod5)) &&
+            (addId == (MkMod 0 :: Mod5)) &&
+            (mulId == (MkMod 1 :: Mod5)) &&
+            (addInv (MkMod 2) == (MkMod 3 :: Mod5))
 
 instance Ring Bool where
         addId  = False
@@ -38,3 +38,27 @@ instance Ring Bool where
 
 instance Parsable Bool where
         parse = listToMaybe . reads
+
+swapIdentities :: RingExpr a -> RingExpr a
+swapIdentities AddId      = MulId
+swapIdentities MulId      = AddId
+swapIdentities a = a
+
+distribute :: RingExpr a -> RingExpr a
+distribute (Mul (Add a b) y) = (Add (Mul (distribute a) (distribute y))
+                                    (Mul (distribute b) (distribute y)))
+distribute (Mul x (Add a b)) = (Add (Mul (distribute x) (distribute a))
+                                    (Mul (distribute x) (distribute b)))
+distribute a = a
+
+squashMulId :: RingExpr a -> RingExpr a
+squashMulId (Mul MulId y) = (squashMulId y)
+squashMulId (Mul x MulId) = (squashMulId x)
+squashMulId a = a
+
+ringMap :: ((RingExpr a) -> (RingExpr a))
+        -> (RingExpr a) -> (RingExpr a)
+ringMap f (Mul x y) = (f (Mul (ringMap f x) (ringMap f y)))
+ringMap f (Add x y) = (f (Add (ringMap f x) (ringMap f y)))
+ringMap f (AddInv x) = (f (AddInv (ringMap f x)))
+ringMap f r = (f r)
