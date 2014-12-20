b0978f199e88dfa85088f00a22b6028cea1cf582
Pancia <anthony.dayzerostudio@gmail.com>
2014-12-20 04:50:37 -0800

    Started Ex6, ynToBool working, parseData basically (?) there.

---
+++ Ex6/Ex6.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
+
+module HW06 where
+
+import Data.Aeson
+import Data.Monoid
+import GHC.Generics
+
+import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.Text                  as T
+import qualified Data.Text.IO               as T
+
+import Control.Monad
+
+ynToBool :: Value -> Value
+ynToBool (String "Y") = Bool True
+ynToBool (String "N") = Bool False
+ynToBool v = v
+
+parseData :: B.ByteString -> Either String Value
+parseData bs = Right $ Object $ fmap ynToBool d
+        where (Object d) = case (eitherDecode bs) of
+                             Left  e -> error e
+                             Right r -> r
+
+testParseData file =
+        B.readFile file >>=
+        return . parseData
